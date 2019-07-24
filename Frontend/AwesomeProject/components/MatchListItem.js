96bac1712ec87417dc923deb03e784eae4eef55f
Marinus <malaue4@gmail.com>
2019-07-24 20:22:06 +0200

    match item

--- Frontend/AwesomeProject/components/MatchListItem.js
+++ Frontend/AwesomeProject/components/MatchListItem.js
@@ -34,12 +34,16 @@ export class MatchListItem extends Component {
     render(){
         return (
             <View style={styles.container}>
-                <Image source={this.state.team1.logo} style={{width:52, height:52, backgroundColor:"#5d79ae"}}></Image>
-                <View style={{flex:1, flexDirection: "column", alignItems:"stretch"}}>
-                    <Text style={{textAlign:"left", backgroundColor:"#5d79ae"}}>{this.state.team1.name}</Text>
-                    <Text style={{textAlign:"right", backgroundColor:"#de9b35"}}>{this.state.team2.name}</Text>
+                <View style={styles.leftBackground}>
+                    <Image source={this.state.team1.logo} style={styles.teamLogo}/>
                 </View>
-                <Image source={this.state.team2.logo} style={{width:52, height:52, backgroundColor:"#5d79ae"}}></Image>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.teamOne}>{this.state.team1.name}</Text>
+                    <Text style={styles.teamTwo}>{this.state.team2.name}</Text>
+                </View>
+                <View style={styles.rightBackground}>
+                    <Image source={this.state.team2.logo} style={styles.teamLogo}/>
+                </View>
             </View>
         )
     }
@@ -47,15 +51,40 @@ export class MatchListItem extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex:1, 
         flexDirection:"row", 
-        height: 52,
-
+        justifyContent: "center",
+        backgroundColor: '#de9b35',
+    },
+    leftBackground:{
+        backgroundColor: '#5d79ae',
+        paddingRight:16,
+    },
+    rightBackground:{
+        paddingLeft:16,
+    },
+    nameContainer:{
+        flex:1, 
+        flexDirection: "column", 
+        alignItems:'stretch',
+        marginHorizontal:-16,
+    },
+    teamLogo:{
+        width:52, 
+        height:52, 
+        margin:4,
     },
     teamOne:{
-        textAlign:"right", backgroundColor:"#de9b35"
+        flex:1,
+        textAlign:"right",
+        textAlignVertical:'center',
+        backgroundColor:"#de9b35",
+        borderBottomLeftRadius:12,
     },
     teamTwo:{
-        textAlign:"left", backgroundColor:"#5d79ae"
+        flex:1,
+        textAlign:"left",
+        textAlignVertical:'center',
+        backgroundColor:"#5d79ae",
+        borderTopRightRadius:12,
     }
 });
